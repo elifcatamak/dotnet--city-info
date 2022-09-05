0c77938263f3536c4eeafacd2422371ec44fd2ae
elifcatamak <elifcatamak@gmail.com>
2022-09-05 18:12:26 +0300

    Content negotiation updated, xml support added FilesController created with GetFile method

---
+++ CityInfo.Api/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.Api.Controllers;
+
+[Route("api/files")]
+[ApiController]
+public class FilesController : ControllerBase
+{
+    private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+    public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+    {
+        _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                                            ?? throw new ArgumentNullException(
+                                                nameof(fileExtensionContentTypeProvider));
+    }
+
+    [HttpGet("{fileId}")]
+    public ActionResult GetFile(string fileId)
+    {
+        // Would use the fileId to fetch the correct file path
+        // However, for now I hardcoded the filePath for practice purposes 
+        var filePath = "blank.pdf";
+
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound();
+        }
+
+        if (!_fileExtensionContentTypeProvider.TryGetContentType(filePath, out var contentType))
+        {
+            contentType = "application/octet-stream";
+        }
+
+        var bytes = System.IO.File.ReadAllBytes(filePath);
+
+        return File(bytes, contentType, Path.GetFileName(filePath));
+    }
+}
