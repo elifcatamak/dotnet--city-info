a3d05ec607449d7f00b892aa4e35f8bf7fbefeb5
elifcatamak <elifcatamak@gmail.com>
2022-09-09 18:41:52 +0300

    mailSettings configuration is now handled in appsettings

--- CityInfo.Api/Services/CloudMailService.cs
+++ CityInfo.Api/Services/CloudMailService.cs
@@ -2,8 +2,14 @@ namespace CityInfo.Api.Services;
 
 public class CloudMailService : IMailService
 {
-    private string _mailTo = "admin@mycompany.com";
-    private string _mailFrom = "noreply@mycompany.com";
+    private readonly string _mailTo = string.Empty;
+    private readonly string _mailFrom = string.Empty;
+
+    public CloudMailService(IConfiguration configuration)
+    {
+        _mailTo = configuration["mailSettings:mailToAddress"];
+        _mailFrom = configuration["mailSettings:mailFromAddress"];
+    }
 
     public void Send(string subject, string message)
     {
