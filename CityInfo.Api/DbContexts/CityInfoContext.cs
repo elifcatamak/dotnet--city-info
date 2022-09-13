3f2b32c34a85b7d2bc6819f3ef3c7a979f5d69a9
elifcatamak <elifcatamak@gmail.com>
2022-09-13 14:51:18 +0300

    Added seed data Added corresponding migration

--- CityInfo.Api/DbContexts/CityInfoContext.cs
+++ CityInfo.Api/DbContexts/CityInfoContext.cs
@@ -11,4 +11,66 @@ public class CityInfoContext : DbContext
     public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>().HasData(
+            new City("New York City")
+            {
+                Id = 1,
+                Description = "The one with that big park"
+            },
+            new City("Antwerp")
+            {
+                Id = 2,
+                Description = "The one with that cathedral that was never really finished."
+            },
+            new City("Paris")
+            {
+                Id = 3,
+                Description = "The one with that big tower"
+            }
+        );
+
+        modelBuilder.Entity<PointOfInterest>().HasData(
+            new PointOfInterest("Central Park")
+            {
+                Id = 1,
+                Description = "The most visited urban park in the United States.",
+                CityId = 1
+            },
+            new PointOfInterest("Empire State Building")
+            {
+                Id = 2,
+                Description = "A 102-story skyscraper located in Midtown Manhattan.",
+                CityId = 1
+            },
+            new PointOfInterest("Cathedral")
+            {
+                Id = 3,
+                Description = "A Gothic style cathedral, conceived by architects Jan and Pieter Appelmans.",
+                CityId = 2
+            },
+            new PointOfInterest("Antwerp Central Station")
+            {
+                Id = 4,
+                Description = "The the finest example of railway architecture in Belgium.",
+                CityId = 2
+            },
+            new PointOfInterest("Eiffel Tower")
+            {
+                Id = 5,
+                Description = "A wrought iron lattice tower on the Champ de Mars, named after engineer Gustave Eiffel.",
+                CityId = 3
+            },
+            new PointOfInterest("The Louvre")
+            {
+                Id = 6,
+                Description = "The world's largest museum.",
+                CityId = 3
+            }
+        );
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
