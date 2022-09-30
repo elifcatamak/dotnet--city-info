b183cb3ac6e58e3c2fcf8be94f9c40b50b912704
elifcatamak <elifcatamak@gmail.com>
2022-09-30 14:37:01 +0300

    Created Authentication controller Implemented Authenticate method that returns a Jwt token with successful authentication Added necessary NuGet packages

---
+++ CityInfo.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,94 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CityInfo.Api.Controllers;
+
+[Route("api/authentication")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    //Will not be used outside of this class
+    public class AuthenticationRequestBody
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+
+    private class CityInfoUser
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+        public CityInfoUser(int userId, string userName, string firstName, string lastName, string city)
+        {
+            UserId = userId;
+            UserName = userName;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+    }
+
+    public AuthenticationController(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
+    [HttpPost("authenticate")]
+    public ActionResult<string> Authenticate(AuthenticationRequestBody authenticationRequestBody)
+    {
+        // Step 1: validate the username/password
+        var user = ValidateUserCredentials(authenticationRequestBody.UserName, authenticationRequestBody.Password);
+
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        // Step 2: create a token
+        var securityKey =
+            new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_configuration["Authentication:SecretForKey"]));
+
+        var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        // The claims that
+        var claimsForToken = new List<Claim>();
+        claimsForToken.Add(new Claim("sub", user.UserId.ToString()));
+        claimsForToken.Add(new Claim("given_name", user.FirstName));
+        claimsForToken.Add(new Claim("family_name", user.LastName));
+        claimsForToken.Add(new Claim("city", user.City));
+
+        var jwtSecurityToken = new JwtSecurityToken(
+            _configuration["Authentication:Issuer"],
+            _configuration["Authentication:Audience"],
+            claimsForToken,
+            DateTime.Now,
+            DateTime.Now.AddHours(1),
+            signingCredentials);
+
+        var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+        return Ok(tokenToReturn);
+    }
+
+    private CityInfoUser ValidateUserCredentials(string? userName, string? password)
+    {
+        // For demo purposes
+        var result = new CityInfoUser(
+            1,
+            userName ?? "",
+            "MyName",
+            "MySurname",
+            "Antwerp");
+
+        return result;
+    }
+}
